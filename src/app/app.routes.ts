5a3abf4965a017f9222b05f37e6db3c3ea646f71
Mariem <mariemmohamed1421@gmail.com>
2024-06-17 06:05:46 +0300

    Fixing Registeration Form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,7 @@ import { MealDetailsComponent } from './meal/meal-details/meal-details.component
 //import { NotfoundComponent } from './notfound/notfound.component';
 import { LoginFormComponent } from './Registeration/login-form/login-form.component';
 import { RegisterFormComponent } from './Registeration/register-form/register-form.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 export const routes: Routes = [
 
 
@@ -39,40 +40,10 @@ export const routes: Routes = [
   //   ]
   // },
 
-  { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "login", component: LoginFormComponent },
+  { path: "", redirectTo: "register", pathMatch: "full" },
   { path: "register", component: RegisterFormComponent },
-
-
-  {path: '', component:ChefMealsComponent},
-  {path: 'chefMeals', component:ChefMealsComponent},
-  {path: 'requestedMeals', component:RequestedMealsComponent},
-]},
-{ path: 'Meals', component: MealListComponent },
-//  {path:'Meals/Details/:id',component:MealDetailsComponent,title:"Details of meal"}
-{path:'Meals/Details',component:MealDetailsComponent,title:"Details of meal"},
-  // {path:'**',component:NotfoundComponent} //component notfound
-
+  { path: "login", component: LoginFormComponent },
   { path: 'ClientForm', component: ClientProfleFormComponent },
-  { path: 'profile', redirectTo: 'profile/Favourites', pathMatch: 'full' },
-
-  {
-    path: 'profile', component: ClientProfleComponent, children: [
-
-      { path: '', component: ClientFavouriteComponent },
-      { path: 'Favourites', component: ClientFavouriteComponent },
-      { path: 'Orders', component: ClientOrderComponent },
-    ]
-  },
-  { path: 'ChefPageForm', component: ChefPageFormComponent },
-
-  { path: 'ChefPage', redirectTo: 'ChefPage/chefMeals', pathMatch: 'full' },
-  {
-    path: 'ChefPage', component: ChefPageComponent, children: [
-
-      { path: '', component: ChefMealsComponent },
-      { path: 'chefMeals', component: ChefMealsComponent },
-      { path: 'requestedMeals', component: RequestedMealsComponent },
-    ]
-  },
-];
+  // { path: "", redirectTo: "favorite", pathMatch: "full" },
+  { path: "favorite", component: FavoriteComponent },
+]
