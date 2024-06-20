4910cb45bab5154501cb435be983d109426149c7
Mariem <mariemmohamed1421@gmail.com>
2024-06-20 22:56:25 +0300

    Header Changes

--- src/app/Core/header/header.component.ts
+++ src/app/Core/header/header.component.ts
@@ -5,8 +5,28 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  clickTimeout: any;
+  singleClickAction(): void {
+    window.open('http://127.0.0.1:7860/', '_blank');
+  }
 
+  doubleClickAction(): void {
+    window.open('http://127.0.0.1:7860/?__theme=dark', '_blank');
+  }
+
+  handleClick(): void {
+    if (this.clickTimeout) {
+      clearTimeout(this.clickTimeout);
+      this.clickTimeout = null;
+      this.doubleClickAction();
+    } else {
+      this.clickTimeout = setTimeout(() => {
+        this.singleClickAction();
+        this.clickTimeout = null;
+      }, 250); // Adjust the delay as needed
+    }
+  }
 }
