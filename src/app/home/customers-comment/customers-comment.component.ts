ef63553a491d3d368cb1f3760b69b25255ca4610
Mariem <mariemmohamed1421@gmail.com>
2024-06-28 10:24:05 +0300

    Editing in Home Page

---
+++ src/app/home/customers-comment/customers-comment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customers-comment',
+  standalone: true,
+  imports: [],
+  templateUrl: './customers-comment.component.html',
+  styleUrl: './customers-comment.component.css'
+})
+export class CustomersCommentComponent {
+
+}
--- src/app/home/top-meals/top-meals.component.ts
+++ src/app/home/top-meals/top-meals.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top-meals',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './top-meals.component.html',
   styleUrl: './top-meals.component.css'
 })
