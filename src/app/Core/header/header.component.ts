5c218f7c3eb71f24860436a6bce3b0f20aa0c582
Dalia Mansour <157371517+DaliaHallabo@users.noreply.github.com>
2024-07-02 12:24:22 +0300

    Commit

--- src/app/Core/header/header.component.ts
+++ src/app/Core/header/header.component.ts
@@ -1,18 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    CommonModule,
+
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   clickTimeout: any;
+  Islogin=localStorage.getItem("token")==null ? false :true;
   singleClickAction(): void {
     window.open('http://127.0.0.1:7860/', '_blank');
   }
-
+constructor(private router :Router , private authService :AuthService){}
   doubleClickAction(): void {
     window.open('http://127.0.0.1:7860/?__theme=dark', '_blank');
   }
@@ -28,5 +36,11 @@ export class HeaderComponent {
         this.clickTimeout = null;
       }, 250); // Adjust the delay as needed
     }
+  }
+  logout(){
+localStorage.clear()
+
+this.router.navigateByUrl("home")
+
   }
 }
